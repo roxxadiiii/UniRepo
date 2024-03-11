3031f215d7eb36d372adf4656d68f7a644ed96cb
Aditya kumar <itsmeadityakumararya@gmail.com>
2024-03-12 00:52:58 +0530

    chaotic added

--- installKeyrings.sh
+++ installKeyrings.sh
@@ -1,61 +1,25 @@
 !/bin/bash
 
+echo "Installing RebornOS and Chaotic keyrings and mirrorlists..."
 
-#installing keyrings of eos
+sudo pacman -U --noconfirm --needed \
+	rebornos-keyring-20231128-1-any.pkg.tar.zst \
+	rebornos-mirrorlist-20240215-1-any.pkg.tar.zst \
+	chaotic-keyring-20230616-1-any.pkg.tar.zst \
+	chaotic-mirrorlist-20240130-1-any.pkg.tar.zst \
+	endeavouros-keyring-20231222-1-any.pkg.tar.zst \
+	endeavouros-mirrorlist-24.2-1-any.pkg.tar.zst
 
-echo " >>>>>>>>     INSTALLING KEYRINGS OF EOS"
+echo "Installation complete."
 
-sudo pacman -U endeavouros-keyring-20231222-1-any.pkg.tar.zst
+# Define the configuration to be added
+CONFIG_chaotic="[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n"
 
-echo
-echo ">>>>>>>>> EOS's KEYRINGS INSTALLED"
-
-echo
-echo
-
-echo ">>>>>>>>> INSTALLING MIRRORLIST OF EOS"
-
-sudo pacman -U endeavouros-mirrorlist-24.2-1-any.pkg.tar.zst
-echo
-echo ">>>>>>>> EOS's MIRRORLIST INSTALLED"
-
-echo
-echo
-
-echo ">>>>>>>>> INSTALLING KEYRINGS OF REBORNOS"
-
-sudo pacman -U rebornos-keyring-20231128-1-any.pkg.tar.zst
-
-echo
-echo ">>>>>>>>> REBORNOS's KEYRINGS INSTALLED"
-
-echo
-echo
-
-
-echo ">>>>>>>>> INSTALLING MIRRORLIST OF REBORNOS"
-
-sudo pacman -U rebornos-mirrorlist-20240215-1-any.pkg.tar.zst
-
-echo
-echo ">>>>>>>>> REBORNOS'S MIRRORLIST INSTALLED"
-echo
-echo
-
-echo ">>>>>>>>> INSTALLING KEYRINGS OF CHAOTIC"
-
-sudo pacman -U chaotic-keyring-20230616-1-any.pkg.tar.zst
-
-echo
-echo ">>>>>>>>> CHAOTIC's KEYRINGS INSTALLED"
-echo
-echo
-
-echo ">>>>>>>>> INSTALLING MIRRORLIST OF CHAOTIC"
-
-sudo pacman -U chaotic-mirrorlist-20240130-1-any.pkg.tar.zst
-echo
-echo ">>>>>>>>> CHAOTIC's MIRRORLIST INSTALLED"
-echo
-echo
-
+# Check if the configuration already exists in pacman.conf
+if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+	# If it doesn't exist, append the configuration to pacman.conf
+	echo -e "$CONFIG_chaotic" | sudo tee -a /etc/pacman.conf >/dev/null
+	echo "Chaotic-AUR repository has been added to pacman.conf."
+else
+	echo "Chaotic-AUR repository already exists in pacman.conf."
+fi
